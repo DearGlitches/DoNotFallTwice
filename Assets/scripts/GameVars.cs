180f12a629fbc1910f74a97dd2b1cd07a2bbef40
adrian.enderlin <adrian.enderlin@edu.hefr.ch>
2018-02-16 02:20:41 +0100

    Image Post-Processing Motion Blur

--- Assets/scripts/GameVars.cs
+++ Assets/scripts/GameVars.cs
@@ -1,9 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.PostProcessing;
 
 public class GameVars : MonoBehaviour
 {
+	public Camera motion_blur_camera;
+	public PostProcessingProfile notdrunk;
+	public PostProcessingProfile littledrunk;
+	public PostProcessingProfile drunk;
+	
 	private int health = 2;
 	[SerializeField] private float maxAlcool = 1f;
 	[SerializeField] private float alcoolLossPerFrame = 0.01f;
@@ -12,8 +18,6 @@ public class GameVars : MonoBehaviour
 	[SerializeField] private float alcoolPerDrink = 0.1f;	// alcohol per drink
 	private float myDrinkTime = 0.0f;
 	private float nextDrink = 0.5f;
-	
-
 	
 	// Use this for initialization
 	void Start ()
@@ -37,9 +41,20 @@ public class GameVars : MonoBehaviour
 		}
 
 		alcool -= alcoolLossPerFrame;
-		if (alcool < 0)
+		if (alcool <= 0)
+		{
 			alcool = 0;
-		
+			motion_blur_camera.GetComponent<PostProcessingBehaviour>().profile = notdrunk;
+		}else if (alcool > 0.4f)
+		{
+			motion_blur_camera.GetComponent<PostProcessingBehaviour>().profile = littledrunk;
+		}
+		else if (alcool > 0.7f)
+		{
+			motion_blur_camera.GetComponent<PostProcessingBehaviour>().profile = drunk;
+		}
+
+
 
 	}
 
