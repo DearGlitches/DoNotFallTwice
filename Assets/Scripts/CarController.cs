811893e08ce177d71130078c8bcd416bec007d96
Owen McGill <mcgillowen@gmail.com>
2018-02-16 14:06:03 +0100

    Start car controller

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -7,6 +7,15 @@ public class CarController : MonoBehaviour
 
 	public GameObject CarPrefab;
 	
+	[Range(0,10)]
+	public int NumberOfCars;
+
+	[Range(0.0f,1.0f)]
+	public float BaseSpeed;
+
+	public Vector3 TopStart;
+	public Vector3 BottomStart;
+	
 	
 	// Use this for initialization
 	void Start () {
